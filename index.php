7c9a6666744345079b78d25d10856f889f413d4a
Roman Kunin <mosceo@gmail.com>
2017-09-09 18:12:06 +1000

    done

--- index.php
+++ index.php
@@ -122,12 +122,27 @@ $days_until_deadline = null;
 
                     <label class="checkbox">
                         <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
-                        <input id="show-complete-tasks" class="checkbox__input visually-hidden" type="checkbox">
+                        <input id="show-complete-tasks" class="checkbox__input visually-hidden" type="checkbox"
+                            <?php if ($show_complete_tasks) print('checked'); ?>>
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
 
                 <table class="tasks">
+                    <?php if ($show_complete_tasks): ?>
+                    <tr class="tasks__item task task--completed">
+                        <td class="task__select">
+                            <label class="checkbox task__checkbox">
+                                <input class="checkbox__input visually-hidden" type="checkbox" checked>
+                                <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
+                            </label>
+                        </td>
+                        <td class="task__date">10.04.2017</td>
+
+                        <td class="task__controls">
+                        </td>
+                    </tr>
+                    <?php endif; ?>
 
                     <!--показывать следующий тег <tr/>, если переменная равна единице-->
                     <tr class="tasks__item task task--completed">
