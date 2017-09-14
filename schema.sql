ee039b910f0ec2d153ed0efe8ba8361ef52f0a64
Roman Kunin <mosceo@gmail.com>
2017-09-14 14:55:49 +1000

    remove comments

--- schema.sql
+++ schema.sql
@@ -1,13 +1,8 @@
-/* Проект */
-
 CREATE TABLE project (
     id   INT AUTO_INCREMENT PRIMARY KEY,
     user INT         NOT NULL,    /* пользователь, создавший проект */
     name VARCHAR(60) NOT NULL
 );
-
-
-/* Задача */
 
 CREATE TABLE task (
     id       INT AUTO_INCREMENT PRIMARY KEY,
@@ -17,19 +12,16 @@ CREATE TABLE task (
     created  DATETIME     NOT NULL,
     finished DATETIME,
     deadline DATETIME,
-    file     VARCHAR(1024),
+    file     VARCHAR(1024)
 );
-
-
-/* Пользователь */
 
 CREATE TABLE user (
     id       INT AUTO_INCREMENT PRIMARY KEY,
     reg_date DATETIME     NOT NULL,
     name     VARCHAR(256) NOT NULL,
     email    VARCHAR(256) NOT NULL,
-    password CHAR(60)     NOT NULL,   
-    contacts VARCHAR(1024),
+    password CHAR(60)     NOT NULL,    /* хешированный */
+    contacts VARCHAR(1024)
 );
 
 /* CREATE UNIQUE INDEX email ON user(email); -- не нужно, потому что задан PRIMARY KEY */
